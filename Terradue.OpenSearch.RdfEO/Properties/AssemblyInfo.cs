f56c4e730517763347f84a9bc63ee128e937c63f
Emmanuel Mathot <emmanuel.mahot@gmail.com>
2014-10-08 10:41:59 +0200

    ready for 1.0.0
    Signed-off-by: Emmanuel Mathot <emmanuel.mahot@gmail.com>

--- Terradue.OpenSearch.RdfEO/Properties/AssemblyInfo.cs
+++ Terradue.OpenSearch.RdfEO/Properties/AssemblyInfo.cs
@@ -5,7 +5,7 @@ using System.Runtime.CompilerServices;
 // Change them to the values specific to your project.
 
 [assembly: AssemblyTitle("Terradue.OpenSearch.RdfEO")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("Rdf Earth Observation plugin for OpenSearch library for .Net")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("Terradue")]
 [assembly: AssemblyProduct("")]
@@ -17,7 +17,8 @@ using System.Runtime.CompilerServices;
 // The form "{Major}.{Minor}.*" will automatically update the build and revision,
 // and "{Major}.{Minor}.{Build}.*" will update just the revision.
 
-[assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.*")]
+[assembly: AssemblyInformationalVersion("1.0.0")]
 
 // The following attributes are used to specify the signing key for the assembly,
 // if desired. See the Mono documentation for more information about signing.
