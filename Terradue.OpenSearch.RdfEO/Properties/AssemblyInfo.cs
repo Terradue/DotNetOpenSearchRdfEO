18c0c87b806b062dc134806dce75870a0eed87d7
Emmanuel Mathot <emmanuel.mathot@gmail.com>
2016-03-10 16:12:50 +0100

    1.3.7

--- Terradue.OpenSearch.RdfEO/Properties/AssemblyInfo.cs
+++ Terradue.OpenSearch.RdfEO/Properties/AssemblyInfo.cs
@@ -16,8 +16,8 @@ using NuGet4Mono.Extensions;
 [assembly: AssemblyAuthors("Emmanuel Mathot")]
 [assembly: AssemblyProjectUrl("https://github.com/Terradue/DotNetOpenSearchRdfEO")]
 [assembly: AssemblyLicenseUrl("https://github.com/Terradue/DotNetOpenSearchRdfEO/blob/master/LICENSE")]
-[assembly: AssemblyVersion("1.3.6.*")]
-[assembly: AssemblyInformationalVersion("1.3.6")]
+[assembly: AssemblyVersion("1.3.7.*")]
+[assembly: AssemblyInformationalVersion("1.3.7")]
 
 // The following attributes are used to specify the signing key for the assembly,
 // if desired. See the Mono documentation for more information about signing.
